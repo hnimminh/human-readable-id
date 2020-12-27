4fb46d82274608a487ead34d1670024e2370ddff
Minh Minh <hnimminh@outlook.com>
2020-12-27 14:09:10 +0700

    add some simple test

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,10 @@
 from hrid import HRID
 
-x = HRID(':', ('codec', 'adjective', 'noun', 'verb', 'adverb'))
-print(x.generate())
+hruuid = HRID()
+print(hruuid.generate())
+
+hruuid1 = HRID(':', ('codec', 'adjective', 'noun', 'verb', 'adverb'))
+print(hruuid1.generate())
+
+hruuid2 = HRID('-', ('number', 'adjective', 'noun', 'verb', 'adverb'))
+print(hruuid2.generate())
