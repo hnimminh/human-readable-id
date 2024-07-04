4b3fe66d5101098fd569e6b2a12327d071303de0
Minh Minh <hnimminh@outlook.com>
2024-07-04 23:23:26 +0200

    v0.2.4 with seed thanks to @scrummyin

--- tests/test.py
+++ tests/test.py
@@ -8,3 +8,10 @@ print(hruuid1.generate())
 
 hruuid2 = HRID('-', ('number', 'adjective', 'noun', 'verb', 'adverb'))
 print(hruuid2.generate())
+
+hruuid3 = HRID('-', ('number', 'adjective', 'noun', 'verb', 'adverb'), seed='unique')
+print(hruuid3.generate())
+hruuid4 = HRID(seed='non-unique')
+print(hruuid4.generate())
+hruuid5 = HRID(seed='non-unique')
+print(hruuid5.generate())
